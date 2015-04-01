de9ae535da124eb3cd5408107dcfac9f3a913df2
Scott Lombard <lombardenator@gmail.com>
2015-04-01 17:03:11 -0400

    Added Logger to file

--- src/main/java/org/scott/Tasks/MessageTask.java
+++ src/main/java/org/scott/Tasks/MessageTask.java
@@ -6,6 +6,8 @@ import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import scala.Tuple2;
 
 import java.io.Serializable;
@@ -15,6 +17,7 @@ import java.util.Map;
  * Created by slombard on 3/29/15.
  */
 public class MessageTask implements Serializable {
+    private static final Logger LOG = LoggerFactory.getLogger(MessageTask.class);
 
 
     public void messageRun(JavaDStream<Map> inStream) {
